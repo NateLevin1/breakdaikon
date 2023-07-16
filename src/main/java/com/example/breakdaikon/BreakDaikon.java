d29ff2260ae230cd9062d790ce85d79633db6ca4
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2023-07-15 23:04:12 -0400

    Explain the purpose of DynCompHackJUnitCommandLineParseResult

--- src/main/java/com/example/breakdaikon/BreakDaikon.java
+++ src/main/java/com/example/breakdaikon/BreakDaikon.java
@@ -4,7 +4,10 @@ import com.example.breakdaikon.org.junit.DynCompHackJUnitCommandLineParseResult;
 
 public class BreakDaikon {
     public static void main(String[] args) {
+        // the below line causes DynComp to think JUnit is running,
+        // we could just run with JUnit but that adds a lot of noise to this example
         DynCompHackJUnitCommandLineParseResult.parse(() -> {
+            // the error results because of this line:
             System.out.println((new TestClass<Integer>() {
                 @Override
                 public Integer test() {
